404101ac1a789ee00a1abcbc7a41636ea0a32dd7
felipe_chagas07 <yourname@email.com>
2024-07-14 12:30:27 -0300

    Projeto da DIO finalizado

--- banco.py
+++ banco.py
@@ -17,12 +17,12 @@ def main():
         if opcao == "d":
             valor = float(input("Informe o valor do depósito: "))
 
-            saldo, extrato = deposito(saldo, valor, extrato)
+            saldo, extrato = depositar(saldo, valor, extrato)
 
         elif opcao == "s":
             valor = float(input("Informe o valor do saque: "))
 
-            saldo, extrato = saque(
+            saldo, extrato = sacar(
                 saldo=saldo,
                 valor=valor,
                 extrato=extrato,
@@ -32,7 +32,7 @@ def main():
             )
 
         elif opcao == "e":
-            extrato(saldo, extrato=extrato)
+            exibir_extrato(saldo, extrato=extrato)
 
         elif opcao == "nu":
             criar_usuario(usuarios)
@@ -65,46 +65,47 @@ def menu():
     [q]\tSair
     => """
     return input(textwrap.dedent(menu))
-   
-    
-    
-def saque (*, saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES):
+
+
+
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+    return saldo, extrato
+
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saldo = valor > saldo
     excedeu_limite = valor > limite
-    excedeu_saques = numero_saques >= LIMITE_SAQUES
+    excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("Operação falhou! Você não tem saldo suficiente.")
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
 
     elif excedeu_limite:
-        print("Operação falhou! O valor do saque excede o limite.")
+        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
 
     elif excedeu_saques:
-         print("Operação falhou! Número máximo de saques excedido.")
+        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
 
     elif valor > 0:
         saldo -= valor
         extrato += f"Saque:\t\tR$ {valor:.2f}\n"
         numero_saques += 1
-        print("\n Saque realizado com sucesso!")
+        print("\n=== Saque realizado com sucesso! ===")
 
     else:
         print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
 
     return saldo, extrato
 
-def deposito (saldo, valor, extrato, /):
-    if valor > 0:
-        saldo += valor
-        extrato += f"Depósito: R$ {valor:.2f}\n"
-        print("\n Depósito realizado com Sucesso!")
 
-    else:
-        print("Operação falhou! O valor informado é inválido.")
-
-    return saldo, extrato
-
-def extrato (saldo, /, *, extrato):
+def exibir_extrato(saldo, /, *, extrato):
     print("\n================ EXTRATO ================")
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
@@ -112,20 +113,20 @@ def extrato (saldo, /, *, extrato):
 
 
 def criar_usuario(usuarios):
-    cpf = input("Digite o CPF (somente número): ")
+    cpf = input("Informe o CPF (somente número): ")
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n Já existe usuário com esse CPF!")
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
         return
 
     nome = input("Informe o nome completo: ")
     data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
-    endereco = input("Informe o endereço (logradouro, número - bairro - cidade/sigla do estado): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
 
     usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
 
-    print("Usuário criado com sucesso!")
+    print("=== Usuário criado com sucesso! ===")
 
 
 def filtrar_usuario(cpf, usuarios):
@@ -153,6 +154,5 @@ def listar_contas(contas):
         """
         print("=" * 100)
         print(textwrap.dedent(linha))
-
 
 main()
